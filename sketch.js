e92bba2078b5f1d21238545f4ebc1855817f0468
PoWerGooS <77084057+PoWerGooS@users.noreply.github.com>
2021-01-25 16:34:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var wall,thickness;
+var bullet,speed,weight;
+
+function setup() {
+  createCanvas(800,400);
+thickness=random
+wall=createSprite(1200,200,thickness,height/2)
+  wall.shapeColor=(80,80,80)
+  speed=random(223,321)
+  weight=random(30,52)
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
